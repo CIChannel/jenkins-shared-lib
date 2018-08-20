32dc01ece126f04ee3d7babbd3422e4cd29c4c7c
Anurag (CES) <asharma@agvance.net>
2018-08-20 11:54:38 -0500

    Modified the Test class

--- src/org/foo/Test.groovy
+++ src/org/foo/Test.groovy
@@ -3,17 +3,16 @@ class Test {
    int temp1;
    String temp2;
 
+   Test(id, name) {
+       this.temp1 = id;
+       this.temp2 = name;
+   }
+
    int getStudentID() {
       return this.temp1;
    }
 	
    String getStudentName() {
       return this.temp2;
-   }
-	
-   static void main(String[] args) {
-      Student st = new Student();
-      st.temp1 = 1;
-      st.temp2 = "Joe"
    } 
 }
