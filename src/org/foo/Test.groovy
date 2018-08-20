b49e7b21f2f15f5935d11cfaacbd90c59dfb01c6
Anurag (CES) <asharma@agvance.net>
2018-08-20 10:42:02 -0500

    Testing with root class

---
+++ src/org/foo/Test.groovy
@@ -0,0 +1,18 @@
+class Test {
+   int temp1;
+   String temp2;
+
+   int getStudentID() {
+      return this.temp1;
+   }
+	
+   String getStudentName() {
+      return this.temp2;
+   }
+	
+   static void main(String[] args) {
+      Student st = new Student();
+      st.temp1 = 1;
+      st.temp2 = "Joe"
+   } 
+}
