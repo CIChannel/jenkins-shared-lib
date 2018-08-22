f7fec324a00f20db1205c68ba80c54d363bef91a
Anurag (CES) <asharma@agvance.net>
2018-08-22 16:29:02 -0500

    Adding LogginService

--- vars/logService.groovy
+++ vars/logService.groovy
@@ -1,29 +1,27 @@
-#!/usr/bin/groovy
-import java.util.logging.Logger;
+// #!/usr/bin/groovy
+// import java.util.logging.Logger;
 
-Logger logger = Logger.getLogger("");
+// void info(caller, msg) {
+//     caller.info("\nINFO: ${msg}\n");
+// }
 
-void info(caller, msg) {
-    caller.info("\nINFO: ${msg}\n");
-}
+// void warning(caller, warning, ex) {
+//     caller.info("\nWARNING: ${warning}\n");
+//     caller.info("\nEXCEPTION: ${exception.getMessage()}\n");
+// }
 
-void warning(caller, warning, ex) {
-    caller.info("\nWARNING: ${warning}\n");
-    caller.info("\nEXCEPTION: ${exception.getMessage()}\n");
-}
-
-void error(caller, msg, exception, isStackTrace = false) {
-    if (caller) {
-        caller.error("\nERROR: ${msg}\n");
-        caller.error("\nEXCEPTION: ${exception.getMessage()}\n");
-        if (isStackTrace) {
-            caller.error("\nSTACKTRCE: ${exception.printStackTrace()}\n");
-        }
-    } else {
-        echo "\nERROR: ${msg}\n";
-        echo "\nEXCEPTION: ${exception.getMessage()}\n";
-        if (isStackTrace) {
-            echo "\nSTACKTRCE: ${exception.printStackTrace()}\n";
-        }
-    }
-}
+// void error(caller, msg, exception, isStackTrace = false) {
+//     if (caller) {
+//         caller.error("\nERROR: ${msg}\n");
+//         caller.error("\nEXCEPTION: ${exception.getMessage()}\n");
+//         if (isStackTrace) {
+//             caller.error("\nSTACKTRCE: ${exception.printStackTrace()}\n");
+//         }
+//     } else {
+//         echo "\nERROR: ${msg}\n";
+//         echo "\nEXCEPTION: ${exception.getMessage()}\n";
+//         if (isStackTrace) {
+//             echo "\nSTACKTRCE: ${exception.printStackTrace()}\n";
+//         }
+//     }
+// }
