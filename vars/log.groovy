4eca1c5336ec710e6bfd0058ae910e455e1a9505
Anurag (CES) <asharma@agvance.net>
2018-08-22 16:45:39 -0500

    Remving logservice

---
+++ vars/log.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/groovy
+import java.util.logging.Logger;
+
+void info(caller, msg) {
+    echo "\nINFO: ${msg}\n";
+}
+
+void warning(caller, warning, ex) {
+    echo "\nWARNING: ${warning}\n";
+    echo "\nEXCEPTION: ${exception.getMessage()}\n";
+}
+
+void error(msg, exception, isStackTrace = false) {
+    echo "\nERROR: ${msg}\n";
+    echo "\nEXCEPTION: ${exception.getMessage()}\n";
+    if (isStackTrace) {
+        echo "\nSTACKTRCE: ${exception.printStackTrace()}\n";
+    }
+}
